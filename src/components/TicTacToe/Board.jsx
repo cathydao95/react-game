64f0a4bec48e347aeee93444f9d2cdf62e6fd93a
Cathy Dao <cathydao95@gmail.com>
2023-08-15 18:49:35 -0700

    Add winCombo and game layout

--- src/components/TicTacToe/Board.jsx
+++ src/components/TicTacToe/Board.jsx
@@ -1,3 +1,88 @@
-const Board = () => {
-  return <div></div>;
+import Box from "./Box";
+import { useState } from "react";
+import "./Board.css";
+
+const Board = ({
+  gameActive,
+  setGameActive,
+  player1,
+  player2,
+  player1Turn,
+  setPlayer1Turn,
+  winCombos,
+}) => {
+  const [boxes, setBoxes] = useState(new Array(9).fill(""));
+  const [marked, setMarked] = useState(new Array(9).fill(false));
+
+  // WHY ISNT CHECK WINNER WORKING??? not taking in account first click
+  const checkWinner = (player) => {
+    winCombos.forEach((combo) => {
+      const [a, b, c] = combo;
+      console.log(a, b, c, boxes[a], boxes[b], boxes[c]);
+      if (boxes[a] === player && boxes[b] === player && boxes[c] === player) {
+        console.log("winner");
+      } else {
+        console.log("no");
+      }
+    });
+  };
+
+  // const markBox = (index) => {
+  //   if (gameActive) {
+  //     let newBoxes = [...boxes];
+  //     let checked = [...marked];
+  //     if (player1Turn && marked[index] !== true) {
+  //       newBoxes[index] = player1;
+  //       setBoxes((prevBoxes) => newBoxes);
+  //       setPlayer1Turn(false);
+  //       checked[index] = true;
+  //       setMarked((prevMarked) => checked);
+  //       checkWinner(player1);
+  //     }
+
+  //     if (!player1Turn && marked[index] !== true) {
+  //       newBoxes[index] = player2;
+  //       setBoxes((prevBoxes) => newBoxes);
+  //       checked[index] = true;
+  //       setMarked((prevMarked) => checked);
+  //       setPlayer1Turn(true);
+  //       checkWinner(player2);
+  //     }
+
+  //     // checkDraw();
+  //   }
+  // };
+
+  const markBox = (index) => {
+    if (gameActive && boxes[index] === "") {
+      let newBoxes = [...boxes];
+      let checked = [...marked];
+      newBoxes[index] = player1Turn ? player1 : player2;
+      setBoxes((prevBoxes) => newBoxes);
+      player1Turn ? setPlayer1Turn(false) : setPlayer1Turn(true);
+      checked[index] = true;
+      setMarked((prevMarked) => checked);
+      // checkWinner()
+
+      // checkDraw();
+    }
+  };
+
+  return (
+    <div className="board">
+      {boxes.map((box, index) => {
+        return (
+          <Box
+            key={index}
+            index={index}
+            gameActive={gameActive}
+            markBox={markBox}
+            box={box}
+          />
+        );
+      })}
+    </div>
+  );
 };
+
+export default Board;
---
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,45 @@
+import Board from "./Board";
+import "./TicTacToe.css";
+import { useState } from "react";
+const TicTacToe = () => {
+  // implement so that user can choose player vs hardcoding
+  const player1 = "X";
+  const player2 = "O";
+  const [player1Turn, setPlayer1Turn] = useState(true);
+  const [gameActive, setGameActive] = useState(true);
+
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  // const handlePlayerSelection = (e) => {
+  //   setPlayer1(e.target.dataset.customValue);
+  // };
+
+  return (
+    <>
+      <main>
+        <h1>Tic Tac Toe</h1>
+        {/* <SelectPlayer handlePlayerSelection={handlePlayerSelection} /> */}
+        <Board
+          gameActive={gameActive}
+          setGameActive={setGameActive}
+          player1={player1}
+          player2={player2}
+          player1Turn={player1Turn}
+          setPlayer1Turn={setPlayer1Turn}
+          winCombos={winCombos}
+        />
+      </main>
+    </>
+  );
+};
+
+export default TicTacToe;
