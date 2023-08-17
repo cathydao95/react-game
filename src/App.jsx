ed734d6f70e1b7da7e3cf602bed3a66d444b7ce0
Cathy Dao <cathydao95@gmail.com>
2023-08-16 21:14:31 -0700

    import game components and set show states

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import { useState } from "react";
-
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+import Wordle from "./components/Wordle/Wordle";
 import "./App.css";
 
 function App() {
+  const reactGames = ["Tic-Tac-Toe", "Wordle"];
+  const [showTTT, setShowTTT] = useState(false);
+  const [showWordle, setShowWordle] = useState(false);
+
+  const showGame = (e) => {
+    let game = e.target.dataset.game;
+
+    if (game === "Tic-Tac-Toe") {
+      setShowTTT(true);
+      setShowWordle(false);
+    } else if (game === "Wordle") {
+      setShowWordle(true);
+      setShowTTT(false);
+    }
+  };
   return (
     <>
-      <h1>React Game</h1>
+      <div
+        className={`games-container ${showTTT || showWordle ? "hide" : "show"}`}
+      >
+        <h1 className="title">React Games</h1>
+        <div className="game-btn-container">
+          {reactGames.map((game) => {
+            return (
+              <button
+                onClick={showGame}
+                key={game}
+                data-game={game}
+                className="game-btn"
+              >
+                {game}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <TicTacToe show={showTTT} setShow={setShowTTT} />
+
+      <Wordle show={showWordle} setShow={setShowWordle} />
     </>
   );
 }
